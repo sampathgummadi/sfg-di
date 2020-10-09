95b82726357ff3a88f272e8c87536e044f9856b6
sampathgummadi <ggs.sampath@gmail.com>
2020-10-09 21:26:23 +0530

    commit

--- src/main/java/com/sampath/sfgdi/SfgDiApplication.java
+++ src/main/java/com/sampath/sfgdi/SfgDiApplication.java
@@ -2,12 +2,21 @@ package com.sampath.sfgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.sampath.sfgdi.controller.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx= SpringApplication.run(SfgDiApplication.class, args);
+		
+		MyController myController = (MyController) ctx.getBean("myController");
+		
+		String greetingString = myController.myString();
+		
+		System.out.println(greetingString);
 	}
 
 }
